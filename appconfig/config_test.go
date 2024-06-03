74be29842e3a2fc3a5b016557056a1507f569930
Maxim Kolchurin <maxim.kolchurin@gmail.com>
2024-06-04 00:49:30 +0300

    fix config test

--- appconfig/config_test.go
+++ appconfig/config_test.go
@@ -2,16 +2,23 @@ package appconfig
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestParseConfig(t *testing.T) {
-	conf, err := GetConfig()
+	conf, err := GetConfig("config_test.yaml")
 	if err != nil {
 		t.Error(err)
 	}
-	if conf == nil {
-		t.Errorf("Conf is nil")
-	}
-	t.Log(conf.S3)
-	t.Log(conf.Database)
+	require.NotNil(t, conf)
+	require.Equal(t, conf.S3.Endpoint, "endpoint")
+	require.Equal(t, conf.S3.AccessKeyID, "accessKeyID")
+	require.Equal(t, conf.S3.SecretAccessKey, "secretAccessKey")
+	require.Equal(t, conf.S3.UseSSL, false)
+	require.Equal(t, conf.Database.Host, "host")
+	require.Equal(t, conf.Database.Port, 1234)
+	require.Equal(t, conf.Database.Username, "username")
+	require.Equal(t, conf.Database.Password, "password")
+	require.Equal(t, conf.Database.DatabaseName, "dbname")
 }
